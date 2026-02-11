ffd7b45d5ae70299a6578705dd724dfd754c9d7a
alvinskylers <alvinskylers.dev@gmail.com>
2026-02-11 10:44:57 +0800

    add: register feature add: module, controller, and service for auth dir fix: prisma scheme and configs

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async register(registerDto: RegisterDto) {
+    const { email, password, name } = registerDto;
+
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+        throw new Error("User already exists");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+      },
+    });
+    const { password: _, ...result } = user;
+    return result;
+  }
+}
