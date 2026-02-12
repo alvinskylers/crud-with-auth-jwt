2de17bc690bffbec74979641480a165dffdb86b1
alvinskylers <alvinskylers.dev@gmail.com>
2026-02-12 16:20:00 +0800

    feat: Product CRUD add: http method get product add: http method get product by id add: http method post product add: http method put product add: http method delete product

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { ProductsService } from './products.service';
-import { Product } from '@prisma/client';
+import type { Product } from '@prisma/client';
 
 @Controller('products')
 export class ProductsController {
@@ -9,5 +9,25 @@ export class ProductsController {
   @Get()
   getProducts(): Promise<Product[] | null> {
     return this.productService.getAllProducts();
+  }
+
+  @Get(':id')
+  getProductById(@Param('id') id: string): Promise<Product | null> {
+    return this.productService.getProductById(id);
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string): Promise<Product> {
+    return this.productService.deleteProduct(id);
+  }
+
+  @Post()
+  createProduct(@Body() createProduct: Product): Promise<Product> {
+    return this.productService.createProduct(createProduct);
+  }
+
+  @Put(':id')
+  updateProduct(@Param('id') id: string, @Body() updateProduct: Product): Promise<Product> {
+    return this.productService.updateProduct(id, updateProduct);
   }
 }
